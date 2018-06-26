352a568300074708b3aa55cf6923eba12932bf5a
mcarreon <carryon2095@gmail.com>
2018-06-26 15:40:26 -0700

    added clock

--- js/app.js
+++ js/app.js
@@ -6,19 +6,18 @@ var userVar = {
 
 var gameVar = {
     gameStarted: false,
-    countTime: 30,
     msg: ['Out of time!', 'Nope!', 'Correct!'],
 
     currentQuest: {},
     nextQuest: {},
     listQuest: [],
     staticListQuest: [{
-            question: 'What is the 3rd letter',
+            question: 'What is the 3rd letter?',
             options: ['a', 'b', 'c', 'd'],
             correct: 'c'
         },
         {
-            question: 'What is the 2rd letter',
+            question: 'What is the 2rd letter?',
             options: ['a', 'b', 'c', 'd'],
             correct: 'b'
         },
@@ -31,20 +30,24 @@ var gameCtrl = {
         if (!gameVar.gameStarted) {
             gameVar.gameStarted = true;
             $('#button-area').html('<button id="start-button">Start</button>');
-        }
-        else {
+        } else {
             $('#button-area').html('<button id="start-button">Would you like to retry?</button>');
         }
-        
+
     },
     //sets up area for question phase
     createQuestionCard: function () {
         var timer = $('<h2>');
-        timer.text('You have ' + gameVar.countTime + ' seconds left.');
+        timer.text('You have ' + clock.countTime + ' seconds left.');
         $('#timer-area').append(timer);
+        clock.startClock();
+
+        var question = $('<h3>');
+        question.text(gameVar.currentQuest.question);
+        $('#question-area').append(question);
 
         for (var i = 0; i < 4; i++) {
-            var answer = $('<h3>');
+            var answer = $('<h4>');
             answer.text(gameVar.currentQuest.options[i]);
             $('#answer-area').append(answer);
         }
@@ -55,18 +58,48 @@ var gameCtrl = {
         if (type === 'current') {
             gameVar.currentQuest = gameVar.listQuest[random];
             console.log(gameVar.currentQuest);
-        }
-        else if (type === 'next') {
+        } else if (type === 'next') {
             gameVar.nextQuest = gameVar.listQuest[random];
             console.log(gameVar.nextQuest);
-        }    
+        }
         gameVar.listQuest.splice(random, 1);
-        
+
         console.log(gameVar.listQuest);
     },
+    //clears areas to prepare 
+    areaClear: function () {
+        $('#timer-area').empty();
+        $('#question-area').empty();
+        $('#answer-area').empty();
+    },
     //creates result card based off of userWon
-    createResultCard: function (bool) {
+    createResultCard: function () {
 
+    },
+}
+
+var clock = {
+    timerStarted: false,
+    countTime: 30,
+
+    startClock: function () {
+        if (!clock.timerStarted) {
+            intervalID = setInterval(clock.countClock, 1000);
+            clock.timerStarted = true;
+        }
+    },
+    countClock: function () {
+        if (clock.countTime === 0) {
+            clock.resetClock();
+            clock.countTime = 30;
+        } else {
+            clock.countTime -= 1;
+            $('h2').text('You have ' + clock.countTime + ' seconds left.');
+        }
+    },
+    resetClock: function () {
+        clearInterval(intervalID);
+        clock.timerStarted = false;
     }
 }
 
@@ -74,7 +107,7 @@ $(document).ready(function () {
     gameCtrl.createStartBtn();
 
 
-    $('#start-button').on('click', function() {
+    $('#start-button').on('click', function () {
         $('#button-area').empty();
         gameVar.listQuest = gameVar.staticListQuest;
 
@@ -82,6 +115,6 @@ $(document).ready(function () {
         gameCtrl.pickQuest('next');
         gameCtrl.createQuestionCard();
 
+
     });
 })
-
